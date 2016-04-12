15c98859b161e2110e2eba8e77a7662150114c9c
Michael Volz (Flynn) <m.volz@anymotion.de>
2016-04-12 11:33:09 +0200

    Initial commit

--- src/xUnitTestOutputHelper/Class1.cs
+++ src/xUnitTestOutputHelper/Class1.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace xUnitTestOutputHelper
 {
@@ -9,8 +12,27 @@ namespace xUnitTestOutputHelper
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class Class1
     {
-        public Class1()
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Class1(ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void OutputTest1()
+        {
+            _testOutputHelper.WriteLine("Some output!");
+
+            false.Should().BeTrue();
+        }
+
+        [Fact]
+        public void OutputTest2()
+        {
+            _testOutputHelper.WriteLine("No output!");
+
+            true.Should().BeTrue();
         }
     }
 }
